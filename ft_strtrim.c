33b0d23c080fc28fc25a5f7ce1bf00f46144f340
tuliokaaz <tuliocaz.windcoast@gmail.com>
2021-06-01 19:38:41 -0300

    add ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,29 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tcamargo <tcamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/28 09:40:28 by tcamargo          #+#    #+#             */
-/*   Updated: 2021/06/01 19:37:26 by tcamargo         ###   ########.fr       */
+/*   Created: 2021/06/01 18:38:28 by tcamargo          #+#    #+#             */
+/*   Updated: 2021/06/01 19:27:21 by tcamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(char const *str, int c)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*s;
+	size_t	start;
+	char	*str;
 
-	s = (char *) str;
-	while (*s)
-	{
-		if (*s == (char)c)
-			return (s);
-		s++;
-	}
-	if (*s == '\0' && c == '\0')
-		return (s);
-	return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	start = ft_strlen(s1);
+	while (start && ft_strchr(set, s1[start]))
+		start--;
+	str = ft_substr(s1, 0, start + 1);
+	return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tcamargo <tcamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 15:11:14 by tcamargo          #+#    #+#             */
-/*   Updated: 2021/06/01 17:54:29 by tcamargo         ###   ########.fr       */
+/*   Updated: 2021/06/01 19:34:08 by tcamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,24 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 void	*ft_memccpy(void *dst, void *src, int c, size_t n);
+size_t	ft_strlcat(char *dst, char const *src, size_t n);
 size_t	ft_strlcpy(char *dst, char const *src, size_t n);
 char	*ft_strnstr(char *str1, char *str2, size_t n);
+char	*ft_strtrim(char const *s1, char const *set);
 int		ft_strncmp(char *str1, char *str2, size_t n);
 void	*ft_memmove(void *dst, void *src, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_memcmp(void *str1, void *str2, size_t n);
-size_t	ft_strlcat(char *dst, char const *src, size_t n);
 void	*ft_memcpy(void *dst, void *str, size_t n);
 void	*ft_memset(void *str, int c, size_t n);
 void	*ft_memchr(void *str, int c, size_t n);
 void	*ft_calloc(size_t nitens, size_t size);
+char	*ft_strchr(char const *str, int c);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_bzero(void *dst, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	*ft_strrchr(char *str, int c);
-char	*ft_strchr(char *str, int c);
 void	ft_putnbr_fd(int n, int fd);
 size_t	ft_strlen(char const *str);
 char	*ft_strdup(char *str);
