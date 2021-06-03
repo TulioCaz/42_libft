683a4812e067a8cf5405c7e443ce5d2c51867918
tuliokaaz <tuliocaz.windcoast@gmail.com>
2021-06-03 18:23:56 -0300

    Bônus: add ft_lstdelone

---
+++ ft_lstdelone.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcamargo <tcamargo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/03 18:12:22 by tcamargo          #+#    #+#             */
+/*   Updated: 2021/06/03 18:22:28 by tcamargo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+	lst = NULL;
+}
