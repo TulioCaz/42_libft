3198bea7e4e20f01a0ed5f4be7a9c629b1ae610b
tuliokaaz <tuliocaz.windcoast@gmail.com>
2021-06-10 12:33:49 -0300

    add

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tcamargo <tcamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 16:45:08 by tcamargo          #+#    #+#             */
-/*   Updated: 2021/06/09 10:33:23 by tcamargo         ###   ########.fr       */
+/*   Updated: 2021/06/10 12:32:08 by tcamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,24 @@
 
 char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
-	size_t	n;
-	char	*sub_s;
+	size_t	i;
+	size_t	j;
+	char	*substr;
 
-	if (!str)
+	substr = (char*)malloc(sizeof(*str) * (len + 1));
+	if (!substr)
 		return (NULL);
-	if ((unsigned int)ft_strlen(str) < start)
-		return (NULL);
-	n = ft_strlen(str + start);
-	if (n < len)
-		len = n;
-	sub_s = malloc(sizeof(char) * (len + 1));
-	if (!sub_s)
-		return (NULL);
-	ft_strlcpy(sub_s, str + start, len + 1);
-	return (sub_s);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (i >= start && j < len)
+		{
+			substr[j] = str[i];
+			j++;
+		}
+		i++;
+	}
+	substr[j] = '\0';
+	return (substr);
 }
