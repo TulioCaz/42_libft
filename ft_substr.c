e66360367bbf21cd335ce2463f8782a92a982d5d
tuliokaaz <tuliocaz.windcoast@gmail.com>
2021-06-10 12:47:37 -0300

    Fix ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tcamargo <tcamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 16:45:08 by tcamargo          #+#    #+#             */
-/*   Updated: 2021/06/10 12:32:08 by tcamargo         ###   ########.fr       */
+/*   Updated: 2021/06/10 12:46:39 by tcamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 	size_t	j;
 	char	*substr;
 
-	substr = (char*)malloc(sizeof(*str) * (len + 1));
+	if (ft_strlen(str) < len)
+		len = ft_strlen(str);
+	if ((unsigned int)ft_strlen(str) < start)
+		return (ft_calloc(1, 1));
+	substr = (char*)ft_calloc(sizeof(*str), (len + 1));
 	if (!substr)
 		return (NULL);
 	i = 0;
