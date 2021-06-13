0028818649897e89e61aa23afd07d13ae4ade949
tuliokaaz <tuliocaz.windcoast@gmail.com>
2021-06-13 17:40:43 -0300

    Fix forbiden function

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tcamargo <tcamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:04:45 by tcamargo          #+#    #+#             */
-/*   Updated: 2021/06/12 11:02:32 by tcamargo         ###   ########.fr       */
+/*   Updated: 2021/06/13 17:32:02 by tcamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strdup(char *str)
 	size_t	len;
 	void	*new;
 
-	len = strlen(str) + 1;
+	len = ft_strlen(str) + 1;
 	new = ft_calloc(sizeof(char), len);
 	if (!new)
 		return (NULL);
