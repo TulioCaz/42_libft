c7b62ee87b998141e025449c5c6526e5517f1975
tuliokaaz <tuliocaz.windcoast@gmail.com>
2021-06-14 19:01:35 -0300

    Fix functions, to use, ft_calloc

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: tcamargo <tcamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 20:21:07 by tcamargo          #+#    #+#             */
-/*   Updated: 2021/06/02 20:25:58 by tcamargo         ###   ########.fr       */
+/*   Updated: 2021/06/14 19:00:18 by tcamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*newlist;
 
-	newlist = malloc(sizeof(t_list));
+	newlist = ft_calloc(sizeof(t_list), 1);
 	if (!newlist)
 		return (NULL);
 	(*newlist).content = content;
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tcamargo <tcamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 17:32:20 by tcamargo          #+#    #+#             */
-/*   Updated: 2021/06/09 13:31:01 by tcamargo         ###   ########.fr       */
+/*   Updated: 2021/06/14 18:58:43 by tcamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strjoin(char const *str1, char const *str2)
 		return ((char *)str1);
 	s1_len = ft_strlen(str1);
 	s2_len = ft_strlen(str2);
-	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	str = ft_calloc(sizeof(char), (s1_len + s2_len + 1));
 	if (!str)
 		return (NULL);
 	ft_strlcpy(str, str1, s1_len + 1);
